7c07d776c7784794212d2d5c22adb5865c66689e
Sherlline-Josephin <109158067+Sherlline-Josephin@users.noreply.github.com>
2023-02-28 14:00:36 +0530

    Update search.js

--- api/search.js
+++ api/search.js
@@ -4,8 +4,8 @@ import bodyparse from 'body-parser'
 const app = express()
 
 const supabase = createClient(
-    'https://cujacxymzdjbanrbeekh.supabase.co',
-'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1amFjeHltemRqYmFucmJlZWtoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc1Njc5MzMsImV4cCI6MTk5MzE0MzkzM30.zVfuYaup44awOccPnIi4hsMYodEWATJJwCqhxn9c9g8'
+    'https://toseezavzckhztifkbnz.supabase.co',
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvc2VlemF2emNraHp0aWZrYm56Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc1NDQ2NTYsImV4cCI6MTk5MzEyMDY1Nn0.KXJg_HU_5m9PsncEilQX59ponsaCTIzwt5p_6bkU6kg'
 );
 app.use(bodyparse.json())
 app.use(
@@ -15,13 +15,7 @@ app.use(
 )
 
 export default async function search(req, res) {
-    // client.query(`Select * from bank_branch where LOWER(branch) like '%${req.query.q.toLowerCase()}%' or lower(address) like '%${req.query.q.toLowerCase()}%' or LOWER(city) like '%${req.query.q.toLowerCase()}%' or LOWER(district) like '%${req.query.q.toLowerCase()}%' or LOWER(states) like '%${req.query.q.toLowerCase()}%' or LOWER(bank_name) like '%${req.query.q.toLowerCase()}%' order by ifsc  limit ${req.query.limit} offset ${req.query.offset}`, (err, result)=>{
-    //     if(!err){
-    //         res.send(result.rows);
-    //     }
-        
-    // });
-    // client.end;
+
     console.log("hi")
         const {data, error} = await supabase
             .from('Bank')
@@ -29,5 +23,11 @@ export default async function search(req, res) {
 .or(`branch.ilike.%${req.query.q.toLowerCase()}%,city.ilike.%${req.query.q.toLowerCase()}%,district.ilike.%${req.query.q.toLowerCase()}%,state.ilike.%${req.query.q.toLowerCase()}%`)
             .order('ifsc',{ascending:true})
 .range(parseInt(req.query.offset),parseInt(req.query.offset)+parseInt(req.query.limit)-1)
-res.send(data)
+        res.send(data)
 }
+
+
+
+
+
+
